f1cd1db0e00948df2c97dbe5ad67e2549241e9d2
Fernando Santos <sombarepam@gmail.com>
2025-03-28 09:48:58 -0300

    feat(operadores-aritmeticos): adiciona cálculo da área do retângulo

---
+++ Operadores_aritimétricos/Soma_Simples.js
@@ -0,0 +1,5 @@
+//Soma simples
+let number1 = 67
+let number2 = 89
+console.log("seu resultado é " + (number1 + number2) + "!😊")
+// Fernando Santos 09:20
---
+++ Operadores_aritimétricos/Área_do_retângulo.js
@@ -0,0 +1,6 @@
+//Área do retângulo
+let base = 40
+let height = 15
+let rectanglearea = base * height
+console.log("A área desse retangulo é de " + rectanglearea + "cm")
+//Fernando Santos 09:33
