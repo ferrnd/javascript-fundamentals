2e3bbfec89260320ed8491f12d561e0ec035b70b
ferrnd <sombarepam@gmail.com>
2025-04-03 15:58:28 -0300

    feat(operadores-logicos): adiciona verificação de números positivos

---
+++ Operadores_Logicos/trueorfalse.js
@@ -0,0 +1,8 @@
+let num1 = 1
+let num2 = 2
+if (num1 > 0 && num2 > 0) {
+    console.log('sim, os números são postivos')
+} else {
+    console.log('Não, os números são negativos')
+}
+//Fernando Santos 15:54
