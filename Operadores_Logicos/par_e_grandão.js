a5bbf8a2e779521e8cefd517041e820372898282
ferrnd <sombarepam@gmail.com>
2025-04-03 16:47:55 -0300

    feat(operadores-logicos): adiciona verificação de par e maior que 10

---
+++ Operadores_Logicos/par_e_grandão.js
@@ -0,0 +1,15 @@
+num = 12
+if (num % 2 === 0) {
+    console.log('Esse número é par!')
+}
+if (num > 10) {
+    console.log('Esse número é maior que 10!')
+}
+if (num % 2 !== 0) {
+    console.log('Esse número não é par.')
+} if (num < 10) {
+    console.log('Esse número não é maior que 10.')
+}
+
+
+
