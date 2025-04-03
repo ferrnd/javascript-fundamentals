856a45eb56a820413dd906e10a198baa65c064ad
ferrnd <sombarepam@gmail.com>
2025-04-03 16:11:16 -0300

    feat(operadores-logicos): adiciona verificação de número maior que 100

---
+++ Operadores_Logicos/maior_que_100.js
@@ -0,0 +1,8 @@
+num1 = 99
+num2 = 43
+num3 = 101
+if (num1 > 100 || num2 > 100 || num3 > 100) {
+    console.log('Um dos números passa de 100!')
+} else {
+    console.log('nenhum dos números passa de 100 :(')
+}
