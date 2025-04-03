c91550f913722b0495bf0450237c615159c3f2d1
ferrnd <sombarepam@gmail.com>
2025-04-03 16:56:59 -0300

    trueorfalse.js

--- Operadores_Logicos/trueorfalse.js
+++ Operadores_Logicos/trueorfalse.js
@@ -1,7 +1,7 @@
 let num1 = 1
 let num2 = 2
 if (num1 > 0 && num2 > 0) {
-    console.log('sim, os números são postivos')
+    console.log('sim, os números são positivos')
 } else {
     console.log('Não, os números são negativos')
 }
