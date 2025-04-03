9f49c7d69344ebeec96ef5592fda749bc56fbfa8
ferrnd <sombarepam@gmail.com>
2025-04-03 16:57:21 -0300

    maior_que_100.js

--- Operadores_Logicos/maior_que_100.js
+++ Operadores_Logicos/maior_que_100.js
@@ -4,5 +4,5 @@ num3 = 101
 if (num1 > 100 || num2 > 100 || num3 > 100) {
     console.log('Um dos números passa de 100!')
 } else {
-    console.log('nenhum dos números passa de 100 :(')
+    console.log('Nenhum dos números passa de 100 :(')
 }
