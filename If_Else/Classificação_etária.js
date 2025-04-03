77bbad9db32f07515d228d876399c74b01f703c2
ferrnd <sombarepam@gmail.com>
2025-04-03 17:40:04 -0300

    feat(if-else): adiciona classificação etária por idade

---
+++ If_Else/Classificação_etária.js
@@ -0,0 +1,10 @@
+age = 16
+if (age >=0 && age <= 12){
+    console.log('Criança')
+} else if (age >= 13 && age <= 17) {
+    console.log('Adolescente')
+} else if (age >= 18 && age <= 59) {
+    console.log('Adulto')
+} else if (age >= 60) {
+    console.log('Idoso')
+}
