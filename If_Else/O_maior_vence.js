94dd6c27a271efa118d8dbf6bf06bfbe0ac8e531
ferrnd <sombarepam@gmail.com>
2025-04-03 17:20:17 -0300

    feat(if-else): adiciona comparação do maior entre dois números

---
+++ If_Else/O_maior_vence.js
@@ -0,0 +1,8 @@
+num1 = 65
+num2 = 66
+if (num1 > num2) {
+    console.log(`O maior de todos é ${num1}`)
+} else if (num2 > num1){
+    console.log(`O maior de todos é ${num2}`)
+}
+//Eu usei "${}" por que acho estéticamente mais bonito
