a6fd3bedf305e30206992d1ced870ceec09310c2
ferrnd <sombarepam@gmail.com>
2025-04-04 00:28:36 -0300

    feat(switch-case): adiciona calculadora básica com operações

---
+++ Switch_Case/Calculadora_básica.js
@@ -0,0 +1,15 @@
+let num = 1;
+switch (num) {
+    case 1:
+        console.log(144 + 231);
+        break;
+    case 2:
+        console.log(144 - 231);
+        break;
+    case 3:
+        console.log(144 * 231);
+        break;
+    case 4:
+        console.log(144 / 231);
+        break;
+}
