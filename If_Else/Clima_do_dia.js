94c5a98ba607618b8973fd171ac27687e2c797a3
ferrnd <sombarepam@gmail.com>
2025-04-03 23:47:13 -0300

    feat(if-else): adiciona classificação de clima por temperatura

---
+++ If_Else/Clima_do_dia.js
@@ -0,0 +1,8 @@
+let temperatura = 19
+if (temperatura < 15){
+    console.log('Frio')
+} else if (temperatura >= 15 && temperatura <=25) {
+    console.log('Agradável')
+} else {temperatura > 25
+    console.log('Quente')
+}
