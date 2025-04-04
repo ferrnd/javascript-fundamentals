75b81ffa4fe953726b8733f9fbb932fb3fa67692
ferrnd <sombarepam@gmail.com>
2025-04-04 00:22:51 -0300

    feat(switch-case): adiciona exibição do mês do ano

---
+++ Switch_Case/Mes_do_ano.js
@@ -0,0 +1,41 @@
+let mês = 12
+switch (mês) {
+    case 1:
+        console.log(`Janeiro é o ${mês} do ano!`);
+        break;
+    case 2:
+        console.log(`Fevereiro é o ${mês} do ano!`);
+        break;
+    case 3:
+        console.log(`Março é o ${mês} do ano!`);
+        break;
+    case 4:
+        console.log(`Abril é o ${mês} do ano!`);
+        break;
+    case 5:
+        console.log(`Maio é o ${mês} do ano!`);
+        break;
+    case 6:
+        console.log(`Junho é o ${mês} do ano!`);
+        break;
+    case 7:
+        console.log(`Julho é o ${mês} do ano!`);
+        break;
+    case 8:
+        console.log(`Agosto é o ${mês} do ano!`);
+        break;
+    case 9:
+        console.log(`Setembro é o ${mês} do ano!`);
+        break;
+    case 10:
+        console.log(`Outubro é o ${mês} do ano!`);
+        break;
+    case 11:
+        console.log(`Novembro é o ${mês} do ano!`);
+        break;
+    case 12:
+        console.log(`Dezembro é o ${mês} do ano!`);
+        break;
+    default:
+        console.log('Mês inválido');
+}
